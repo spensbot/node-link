e5a41f1350777fb1b413448de701fb8ff1b66172
Spenser Saling <spenser0saling@gmail.com>
2021-01-19 17:56:30 -0800

    Update stuff

--- build/Release/.deps/Release/obj.target/node-link-native/src/node_link.o.d
+++ build/Release/.deps/Release/obj.target/node-link-native/src/node_link.o.d
@@ -1,4 +1,4 @@
-cmd_Release/obj.target/node-link-native/src/node_link.o := /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++ '-DNODE_GYP_MODULE_NAME=node-link-native' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DLINK_PLATFORM_MACOSX=1' '-DBUILDING_NODE_EXTENSION' -I/Users/Spenser/Library/Caches/node-gyp/12.14.0/include/node -I/Users/Spenser/Library/Caches/node-gyp/12.14.0/src -I/Users/Spenser/Library/Caches/node-gyp/12.14.0/deps/openssl/config -I/Users/Spenser/Library/Caches/node-gyp/12.14.0/deps/openssl/openssl/include -I/Users/Spenser/Library/Caches/node-gyp/12.14.0/deps/uv/include -I/Users/Spenser/Library/Caches/node-gyp/12.14.0/deps/zlib -I/Users/Spenser/Library/Caches/node-gyp/12.14.0/deps/v8/include -I/Users/Spenser/Documents/Dev/node-link/node_modules/node-addon-api -I../lib/link/include -I../lib/link/modules/asio-standalone/asio/include  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/node-link-native/src/node_link.o.d.raw   -c -o Release/obj.target/node-link-native/src/node_link.o ../src/node_link.cc
+cmd_Release/obj.target/node-link-native/src/node_link.o := /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++ -o Release/obj.target/node-link-native/src/node_link.o ../src/node_link.cc '-DNODE_GYP_MODULE_NAME=node-link-native' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DLINK_PLATFORM_MACOSX=1' '-DBUILDING_NODE_EXTENSION' -I/Users/Spenser/Documents/Dev/node-link/8.3.4/include/node -I/Users/Spenser/Documents/Dev/node-link/8.3.4/src -I/Users/Spenser/Documents/Dev/node-link/8.3.4/deps/openssl/config -I/Users/Spenser/Documents/Dev/node-link/8.3.4/deps/openssl/openssl/include -I/Users/Spenser/Documents/Dev/node-link/8.3.4/deps/uv/include -I/Users/Spenser/Documents/Dev/node-link/8.3.4/deps/zlib -I/Users/Spenser/Documents/Dev/node-link/8.3.4/deps/v8/include -I/Users/Spenser/Documents/Dev/node-link/node_modules/node-addon-api -I../lib/link/include -I../lib/link/modules/asio-standalone/asio/include  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/node-link-native/src/node_link.o.d.raw   -c
 Release/obj.target/node-link-native/src/node_link.o: ../src/node_link.cc \
   ../src/node_link.h ../lib/link/include/ableton/Link.hpp \
   ../lib/link/include/ableton/platforms/Config.hpp \
@@ -466,10 +466,10 @@ Release/obj.target/node-link-native/src/node_link.o: ../src/node_link.cc \
   ../lib/link/include/ableton/Link.ipp \
   ../lib/link/include/ableton/link/Phase.hpp \
   /Users/Spenser/Documents/Dev/node-link/node_modules/node-addon-api/napi.h \
-  /Users/Spenser/Library/Caches/node-gyp/12.14.0/include/node/node_api.h \
-  /Users/Spenser/Library/Caches/node-gyp/12.14.0/include/node/js_native_api.h \
-  /Users/Spenser/Library/Caches/node-gyp/12.14.0/include/node/js_native_api_types.h \
-  /Users/Spenser/Library/Caches/node-gyp/12.14.0/include/node/node_api_types.h \
+  /Users/Spenser/Documents/Dev/node-link/8.3.4/include/node/node_api.h \
+  /Users/Spenser/Documents/Dev/node-link/8.3.4/include/node/js_native_api.h \
+  /Users/Spenser/Documents/Dev/node-link/8.3.4/include/node/js_native_api_types.h \
+  /Users/Spenser/Documents/Dev/node-link/8.3.4/include/node/node_api_types.h \
   /Users/Spenser/Documents/Dev/node-link/node_modules/node-addon-api/napi-inl.h \
   /Users/Spenser/Documents/Dev/node-link/node_modules/node-addon-api/napi-inl.deprecated.h
 ../src/node_link.cc:
@@ -940,9 +940,9 @@ Release/obj.target/node-link-native/src/node_link.o: ../src/node_link.cc \
 ../lib/link/include/ableton/Link.ipp:
 ../lib/link/include/ableton/link/Phase.hpp:
 /Users/Spenser/Documents/Dev/node-link/node_modules/node-addon-api/napi.h:
-/Users/Spenser/Library/Caches/node-gyp/12.14.0/include/node/node_api.h:
-/Users/Spenser/Library/Caches/node-gyp/12.14.0/include/node/js_native_api.h:
-/Users/Spenser/Library/Caches/node-gyp/12.14.0/include/node/js_native_api_types.h:
-/Users/Spenser/Library/Caches/node-gyp/12.14.0/include/node/node_api_types.h:
+/Users/Spenser/Documents/Dev/node-link/8.3.4/include/node/node_api.h:
+/Users/Spenser/Documents/Dev/node-link/8.3.4/include/node/js_native_api.h:
+/Users/Spenser/Documents/Dev/node-link/8.3.4/include/node/js_native_api_types.h:
+/Users/Spenser/Documents/Dev/node-link/8.3.4/include/node/node_api_types.h:
 /Users/Spenser/Documents/Dev/node-link/node_modules/node-addon-api/napi-inl.h:
 /Users/Spenser/Documents/Dev/node-link/node_modules/node-addon-api/napi-inl.deprecated.h:
